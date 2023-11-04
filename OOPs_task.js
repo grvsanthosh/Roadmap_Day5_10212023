9e3dca10b0687ada3c44965d3675aa82eb8ed1d8
grvsanthosh <grvsanthosh@gmail.com>
2023-11-04 19:45:20 +0530

    Updated circle and movie problems

--- OOPs_task.js
+++ OOPs_task.js
@@ -6,23 +6,85 @@ class Movie {
         this.studio = studio;
         this.rating = rating;
     }
+    // tried with filter array method
+    // gettPG(title,studio,rating) {
+    //     if (this.rating === "PG"){
+    //         return `Movie name : ${this.title} from studio : ${this.studio} with rating : ${this.rating}`;
+    //     }
+    //     else {
+    //         return `Rating is not exact PG`;
+    //     }
+    // }
 
-    gettPG(title,studio,rating) {
-        if (this.rating === "PG"){
-            return `Movie name : ${this.title} from studio : ${this.studio} with rating : ${this.rating}`;
-        }
-        else {
-            return `Rating is not exact PG`;
-        }
+    getPG(movies){
+        return movies.filter((val) => val.rating === 'PG');
     }
 }
 
-const hollywood = new Movie('Casino Royale','Eon Productions','PG­13');
-console.log(hollywood.gettPG());
+const Casino = new Movie('Casino Royale','Eon Productions','PG13');
+const LEO = new Movie('LEO','sun pictures','PG');
+const kathi = new Movie('kathi','LYCA','PG');
+console.log(Casino.getPG([Casino, LEO, kathi]));
 
 // https://github.com/rvsp/typescript-oops/blob/master/Practice/class-circle.md
 
+class Circle {
+    constructor(radius, color){
+        this.Radius = radius;
+        this.Color = color;
+    }
 
+    getRadius(){
+        return `radius: ${this.Radius.toFixed(2)}`;
+    }
+
+    setRadius(radiusval){
+        this.Radius = radiusval;
+        return `radius value is set to ${this.Radius}`;
+    }
+
+    getColor(){
+        return ` color: ${this.Color}`;
+    }
+
+    setColor(colorval){
+        this.Color = colorval;
+        return `color value is set to ${this.Color}`;
+    }
+    toString(){
+        return `Both radius: ${this.Radius.toFixed(2)} and color: ${this.Color}`;
+    }
+
+    getArea(){
+        return ` Area: ${(3.14*(this.Radius.toFixed(2)**2)).toFixed(2)}`;
+    }
+
+    getCircumference(){
+       return `Circumference: ${(2*3.14*this.Radius.toFixed(2)).toFixed(2)}`; 
+    }
+
+
+}
+
+const c1 = new Circle(2.3,"red");
+const c2 = new Circle(3,"black");
+const c3 = new Circle(5,"green");
+const c4 = new Circle(4.3,"blue");
+
+console.log(c1.getRadius());
+console.log(c1.getColor());
+console.log(c1.toString());
+console.log(c1.getArea());
+console.log(c1.getCircumference());
+
+// post setting up values
+console.log(c1.setRadius(2));
+console.log(c1.setColor("orange"));
+console.log(c1.getRadius());
+console.log(c1.getColor());
+console.log(c1.toString());
+console.log(c1.getArea());
+console.log(c1.getCircumference());
 // Write a “person” class to hold all the details.
 
 class Person {
